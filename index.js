88c156d7ef85ada4e10d6faec1f317111bff8e7e
Ioannis Kokkinidis <sudoplz@gmail.com>
2017-07-05 18:08:16 +0300

    Ability to add a prefix to all the events logged.

--- index.js
+++ index.js
@@ -18,15 +18,18 @@ class Amplitude {
   /**
    * Creates a new Amplitude client
    */
-  constructor(apiKey, trackSessionEvents) {
+  constructor(apiKey, trackSessionEvents, eventPrefix) {
     if (apiKey && typeof apiKey === 'string') {
       if (RNAmplitudeSDK) {
+        if (eventPrefix) {
+          this.evPrefix = eventPrefix;
+        }
         RNAmplitudeSDK.initialize(apiKey, trackSessionEvents === true);
       } else {
-        throw new Error('Bugsnag: No native client found. Is BugsnagReactNative installed in your native code project?');
+        throw new Error('RNAmplitudeSDK: No native client found. Is RNAmplitudeSDK installed in your native code project?');
       }
     } else {
-      throw new Error('Bugsnag: A client must be constructed with an API key or Configuration');
+      throw new Error('RNAmplitudeSDK: A client must be constructed with an API key. i.e new Amplitude(key);');
     }
   }
 
@@ -46,15 +49,20 @@ class Amplitude {
     return RNAmplitudeSDK.clearUserProperties();
   }
 
+  setLogEventPrefix(prefix) {
+    this.evPrefix = prefix;
+  }
+
   // --------------------------------------------------
   // Track
   // --------------------------------------------------
 
   logEvent(name, properties) {
+    var eventName = this.evPrefix ? this.evPrefix + name : name;
     if (properties) {
-      return RNAmplitudeSDK.logEventWithProps(name, properties);
+      return RNAmplitudeSDK.logEventWithProps(eventName, properties);
     } else {
-      return RNAmplitudeSDK.logEvent(name);
+      return RNAmplitudeSDK.logEvent(eventName);
     }
   }
 
