f61cb07c857585eebfdce2ff0b9cb34e95af3468
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2017-12-15 20:32:59 +0100

    Update AmplitudeSDK.h
    fix headers when imported from Cocoapods

--- ios/AmplitudeSDK.h
+++ ios/AmplitudeSDK.h
@@ -1,6 +1,22 @@
+
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
+
+#if __has_include(<Amplitude/Amplitude.h>)
 #import <Amplitude/Amplitude.h>
+#else
+#import "Amplitude.h"
+#endif
+
+
+#if __has_include(<Amplitude/AMPRevenue.h>)
 #import <Amplitude/AMPRevenue.h>
+#else
+#import "AMPRevenue.h"
+#endif
 
 @interface RNAmplitudeSDK : NSObject <RCTBridgeModule>
 
