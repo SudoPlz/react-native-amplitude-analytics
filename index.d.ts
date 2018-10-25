b1ae56f871ca6507de5f6a8d6000f3b52a1c1f8f
Hugo Dozois <hugo.dozois@gmail.com>
2018-10-25 15:52:15 -0700

    Add typescript typings
    - index.d.ts is a mirror of index.js
    - package.json points to the typing file

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+export default class Amplitude {
+  constructor(apiKey: string, trackSessionEvents?: boolean, eventPrefix?: string);
+
+  // --------------------------------------------------
+  // Identify
+  // --------------------------------------------------
+  setUserId(userId: string | number | null): void;
+  setUserProperties(properties: Record<string, any>): void;
+  setOptOut(optOut: boolean): void;
+  clearUserProperties(): void;
+  getDeviceId(): Promise<string>;
+  regenerateDeviceId(): void;
+  setLogEventPrefix(prefix: string): void;
+
+  // --------------------------------------------------
+  // Track
+  // --------------------------------------------------
+
+  logEvent(name: string, properties?: Record<string, any>): void;
+  logEventWithTimestamp(name: string, timestamp: number, properties?: Record<string, any>): void;
+
+  // --------------------------------------------------
+  // Revenue
+  // --------------------------------------------------
+  logRevenue(productIdentifier: string, quantity: number, amount: number, receipt?: string): void;
+  addToUserProperty(property: string, amount: number): void;
+  setUserPropertyOnce(property: string, value: string | number | null): void;
+}
